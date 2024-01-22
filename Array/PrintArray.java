10bd6f3fe70cf0e6cfe74188ff9bc847a3bf4fa1
Tanmay <tanmayngandhi1996@gmail.com>
2024-01-22 16:23:48 +0530

    Print elements of Array.

---
+++ Array/PrintArray.java
@@ -0,0 +1,15 @@
+public class PrintArray {
+
+public static void main(String[] args) {
+
+int[] arr = {1, 2, 3, 4, 5, 6};
+
+System.out.print("Element of Array are: ");
+
+for (int i = 0; i < arr.length; i++) {
+    System.out.print(arr[i]+" ");
+        }
+
+    }
+
+}
