b59d958942001d1923faada7368039db44af4ff8
Tanmay <tanmayngandhi1996@gmail.com>
2024-03-08 12:13:15 +0530

    Selection_Sort

---
+++ DSA_Sheet/SelectionSort.java
@@ -0,0 +1,45 @@
+/* 
+    Sort an array of 0s, 1s and 2s
+
+    Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+    Input: 
+    N = 5
+    arr[]= {​​​​​​0 2 1 2 0}​​​​​​
+    Output:
+    0 0 1 2 2
+    Explanation:
+    0s 1s and 2s are segregated 
+    into ascending order.
+*/
+
+public class SelectionSort {
+
+    public static void main(String args[]) {
+      
+        int[] arr = {0, 1, 1, 0, 1, }; // 2, 1, 2, 0, 0, 0, 1};
+    
+        int start = 0;
+    
+        for (int i=0; i < arr.length; i++) {
+            
+            start = i;
+            
+            for (int j = i+1; j < arr.length; j++) {
+                
+                if (arr[start] > arr[j]) {
+                    
+                    start = j;
+                }
+                
+            }
+            
+            int temp = arr[i];
+            arr[i] = arr[start];
+            arr[start] = temp;
+            
+        }
+      
+    }
+    
+}
